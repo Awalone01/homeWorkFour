61aa2fa3b5ddc804e30eb6533ab8c9fe95ba3eaf
Александр <sashaneiv01@yandex.ru>
2021-11-25 22:20:05 +0300

    homeWorkFour Commit 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,33 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //задача 1
+        int i = 0;
+
+	    while (i < 10){
+            i++;
+            System.out.print(i);
+        }
+        System.out.println();
+	    for (int b = 10; b > 0; b--) {
+            System.out.print(b);
+        }
+        System.out.println();
+
+	    //задача 2
+        int friday = 5;
+
+	    for (; friday < 31; friday = friday + 7) {
+            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
+        }
+
+	    //задача 3
+        int year;
+
+        for (year = 1821; year < 2121; year = year + 79) {
+            System.out.println(year);
+        }
+
     }
 }
